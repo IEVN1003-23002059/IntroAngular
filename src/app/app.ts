7fb1df233bd53b7107eb32b91a8f088859302e01
Yao Alvarez <85313@alumnos.utleon.edu.mx>
2026-09-11 18:54:15 -0600

    Forms Module 11/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css',
-  templateUrl: './app.html',
+
 })
 export class App {
 
